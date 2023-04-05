95b9c35f3b74cc0bf325b417bdbc5124691d2f6f
Alex Lee <97260291+The0n1@users.noreply.github.com>
2023-04-04 23:37:08 -0700

    Updated banner

--- splitbill-calculator.py
+++ splitbill-calculator.py
@@ -8,23 +8,29 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
-print("Welcome to the tipping calculator!\n")
+print('''
+mmmmmmm mmmmm  mmmmm           mmm    mm   m        mmm  m    m m        mm  mmmmmmm  mmmm  mmmmm 
+   #      #    #   "#        m"   "   ##   #      m"   " #    # #        ##     #    m"  "m #   "#
+   #      #    #mmm#"        #       #  #  #      #      #    # #       #  #    #    #    # #mmmm"
+   #      #    #             #       #mm#  #      #      #    # #       #mm#    #    #    # #   "m
+   #    mm#mm  #              "mmm" #    # #mmmmm  "mmm" "mmmm" #mmmmm #    #   #     #mm#  #    "
+''')
 
 # Gathering input from user
 total_bill = float(input("How much was the bill? $"))
 total_tip_percent = int(
     input("How much would you like to tip (10, 12, 15, etc)? "))
-sales_tax_percent = float(input("What is the sales tax (8.00, 8.25, 9.00, etc)? "))
+#sales_tax_percent = float(input("What is the sales tax (8.00, 8.25, 9.00, etc)? "))
 total_guest = int(input("How many people are you splitting the bill with? "))
 
 # Calculating how much tip
 total_tip = float(total_bill * total_tip_percent / 100)
 
 # Calculating tax
-total_sales_tax = float(total_bill * sales_tax_percent / 100)
+#total_sales_tax = float(total_bill * sales_tax_percent / 100)
 
 # Calculating how much each guest owes
-final_payment = round((total_bill + total_tip + total_sales_tax) / total_guest,
-                      2)
+#final_payment = round((total_bill + total_tip #+ total_sales_tax) / total_guest, 2)
+final_payment = round((total_bill + total_tip) / total_guest, 2)
 
 print(f"\nEach person should pay... ${final_payment:.2f}")
