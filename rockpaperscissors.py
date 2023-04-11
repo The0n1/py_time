62eec514fa654c530b6bd05d0e20889ad809cab8
Alex Lee <97260291+The0n1@users.noreply.github.com>
2023-04-10 23:28:54 -0700

    Revert "cleaned up rockpaper program"
    This reverts commit 67acdff5776c72dccc2b6f4ee672896f62c18fe0.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,7 +1,6 @@
 import random
 
 rock = '''
-ROCK!!!
     _______
 ---'   ____)
       (_____)
@@ -11,7 +10,6 @@ ROCK!!!
 '''
 
 paper = '''
-PAPER!!!
     _______
 ---'   ____)____
           ______)
@@ -21,7 +19,6 @@ PAPER!!!
 '''
 
 scissors = '''
-SCISSORS!!!
     _______
 ---'   ____)____
           ______)
@@ -33,24 +30,30 @@ SCISSORS!!!
 game_images = [rock, paper, scissors]
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
 if user_choice >= 3 or user_choice < 0: 
   print("You typed an invalid number, you lose!") 
-else:
-  print(game_images[user_choice])
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
 
-  computer_choice = random.randint(0, 2)
-  print("= = = = = = = = = = = = = = = = = = \nComputer choses:")
-  print(game_images[computer_choice])
-
-  if user_choice >= 3 or user_choice < 0: 
-    print("You typed an invalid number, you lose!") 
-  elif user_choice == 0 and computer_choice == 2:
-    print("You win!")
-  elif computer_choice == 0 and user_choice == 2:
-    print("You lose")
-  elif computer_choice > user_choice:
-    print("You lose")
-  elif user_choice > computer_choice:
-    print("You win!")
-  elif computer_choice == user_choice:
-    print("It's a draw")
+####### Debugging challenge: #########
+#Try running this code and type 5.
+#It will give you an IndexError and point to line 32 as the issue.
+#But on line 38 we are trying to prevent a crash by detecting
+#any numbers great than or equal to 3 or less than 0.
+#So what's going on?
+#Can you debug the code and fix it?
+#Solution: https://repl.it/@appbrewery/rock-paper-scissors-debugged-end
